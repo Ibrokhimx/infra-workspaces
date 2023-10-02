949fdbc3cb687604f1a78a0f44bbbfe67dc91757
Ibrokhimx <142237583+Ibrokhimx@users.noreply.github.com>
2023-10-01 22:28:52 -0400

    fixed file tracking issue

---
+++ workspaces/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  cloud {
+    organization = "pitt412"
+
+    workspaces {
+      name = "infra-"
+    }
+  }
+
+  required_providers {
+    tfe = {
+      version = "~> 0.48.0"
+    }
+  }
+}
+
+provider "tfe" {
+  version = "~> 0.48.0"
+}
+
+data "tfe_organization" "summer-cloud" {
+  name = "pitt412"
+}
+
+locals {
+    exec_type = "local"
+    infra_components = [
+        "vpc",
+        "subnet",
+        "ec2",
+    ]
+}
+
+resource "tfe_workspace" "test" {
+  for_each       = toset(local.infra_components)
+  name           = each.key
+  organization   = data.tfe_organization.summer-cloud.name
+  execution_mode = local.exec_type
+}
