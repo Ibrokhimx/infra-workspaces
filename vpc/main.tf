c39d37aa618fc8f5223a3ea3614dc7bca54e6701
Ibrokhimx <142237583+Ibrokhimx@users.noreply.github.com>
2023-10-01 21:27:45 -0400

    created directories

---
+++ vpc/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "pitt412"
+
+    workspaces {
+      prefix = "infra-"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
