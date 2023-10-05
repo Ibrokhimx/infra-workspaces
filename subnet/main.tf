7ce8acdb647ff682730649c5dfb3dd62f101a2db
Ibrokhimx <142237583+Ibrokhimx@users.noreply.github.com>
2023-10-04 20:48:55 -0400

    subnet varfile

--- subnet/main.tf
+++ subnet/main.tf
@@ -14,3 +14,47 @@ terraform {
     }
   }
 }
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+# module aws_vpc{
+#   source = "../vpc/"
+
+# }
+#locals {
+#vpc_id = module.aws_vpc.vpc_id
+
+#}
+
+variable "vpc_id" {
+  default = "vpc-0f1622f820f66d774"
+
+}
+data "aws_vpc" "main" {
+  #id = local.vpc_id
+  #id = module.aws_vpc.vpc_id
+  id = var.vpc_id
+}
+
+variable "subnets" {
+  type = map(object({
+    cidr_block = string
+
+  }))
+  default = {}
+}
+
+
+
+resource "aws_subnet" "main" {
+  for_each   = var.subnets
+  vpc_id     = data.aws_vpc.main.id
+  cidr_block = each.value.cidr_block #cidrsubnet(data.aws_vpc.main.cidr_block, 4, 1)
+
+  tags = {
+    Name = each.key
+  }
+}
---
+++ subnet/tvariables.tfvars
@@ -0,0 +1,12 @@
+subnets = {
+  my-demo1-snet = {
+    cidr_block = "10.0.1.0/24"
+  }
+  my-demo2-snet = {
+    cidr_block = "10.0.2.0/24"
+  }
+  my-demo3-snet = {
+    cidr_block = "10.0.3.0/24"
+  }
+
+}
